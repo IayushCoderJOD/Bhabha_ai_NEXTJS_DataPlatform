800d5b0cdd38767320b54b4d1e8eb0f55dfd914a
Ayush Tyagi <86391985+IayushCoderJOD@users.noreply.github.com>
2024-05-28 10:14:02 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <>
      <div  className="w-[84%] absolute right-0 flex justify-center  items-center">
         <div className='w-[70%] text-xl font-semibold '>
-          <h1>This  is the texting site and the live site is <a href="https://dataformer-bhabha-ai.vercel.app/" >HERE</a></h1>
+          <h4>This  is the texting site and the live site is <a className="text-xl text-blue-800" href="https://dataformer-bhabha-ai.vercel.app/" >HERE</a></h4>
         </div>
     </div>
     </>
